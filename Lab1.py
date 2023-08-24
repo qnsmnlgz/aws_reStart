de8c564ca4387f7d330a9d752f38713a5e284cd1
qnsmnlgz <143053623+qnsmnlgz@users.noreply.github.com>
2023-08-24 10:59:22 +0800

    Add files via upload

---
+++ Lab1.py
@@ -0,0 +1,27 @@
+print("Hello, World")
+print("Python has three numberic types: int, float, and complex")
+
+myValue=1
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is the data type " + str(type(myValue)))
+
+myValue=3.14
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is the data type " + str(type(myValue)))
+
+myValue=5j
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is the date type " + str(type(myValue)))
+
+myValue=True
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is the data type " + str(type(myValue)))
+
+myValue=False
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is the data type " + str(type(myValue)))
